e7c365208d3df53a8bcbcb7530bedc5cf9f546e2
Karthik290902 <92994171+Karthik290902@users.noreply.github.com>
2024-02-24 15:51:45 -0330

    CardDeal Section The CardDeal section has beeen successfully implemented!

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+        Discovering a superior card deal is a breeze with our user-friendly
+        platform. In just a few easy steps, explore and compare credit card
+        options to find the perfect fit for your financial needs, ensuring you
+        get the best value and benefits available.
+      </p>
+      <Button styles="mt-10"/>
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]"></img>
+    </div>
+  </section>
+);
 
-export default CardDeal
+export default CardDeal;
